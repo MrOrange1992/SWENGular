4cadd5fc3e42ce528b209ff1e96f391052de72fd
MrOrange1992 <felix.rauchenwald@edu.fh-joanneum.at>
2017-12-18 12:14:01 +0100

    Created base module/component/entities structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { MovieSearchComponent } from './movie-search/movie-search.component';
+import {UserRegistrationComponent} from './user-registration/user-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationComponent,
+    MovieSearchComponent
   ],
   imports: [
     BrowserModule
